f4ce2bf8838b1b052d0abbb7ae181842253b878f
Troj Andersen <trojandersen@gmail.com>
2024-10-22 17:35:57 +0200

    init

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h2 className='loading-title'>INTERVAL</h2>
+      <p className='loading-text'>For all your timing needs</p>
+    </>
+  )
+}
+
+export default App
