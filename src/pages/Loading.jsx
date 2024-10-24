3819553e6634d2f83f179df5eab44f9980036d07
Troj Andersen <trojandersen@gmail.com>
2024-10-25 01:57:55 +0200

    small changes

--- src/pages/Loading.jsx
+++ src/pages/Loading.jsx
@@ -7,7 +7,16 @@ function Loading() {
   return (
     <main className="loading">
       <Link to="/set-timer">
-        <motion.img src={logoWhite} />
+        <motion.img
+          src={logoWhite}
+          initial={{ opacity: 0, scale: 0.5, x: "150%" }}
+          animate={{ opacity: 1, scale: 1, x: 0 }}
+          transition={{
+            duration: 1,
+            delay: 0.33,
+            ease: [0, 0.66, 0.33, 1],
+          }}
+        />
       </Link>
       <h2 className="loading__title">INTERVAL</h2>
       <p className="loading__text">For all your timing needs</p>
