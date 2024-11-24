da020c031641c9480194de94e0728a3f828c12fd
Troj Andersen <trojandersen@gmail.com>
2024-11-24 22:25:15 +0100

    Analog Clock properly animated

--- src/components/AnalogClock.jsx
+++ src/components/AnalogClock.jsx
@@ -1,46 +1,42 @@
 import clockFace from "../assets/clock.svg";
-import clockHourHandle from "../assets/clockHourHandle.svg";
 import clockMinuteHandle from "../assets/clockMinuteHandle.svg";
+import clockSecondHandle from "../assets/clockSecondHandle.svg";
 import { motion } from "framer-motion";
 import { useContext } from "react";
 import { TimerContext } from "../contexts/TimerContext";
 
 function AnalogClock() {
-  const { hours, minutes } = useContext(TimerContext);
+  const { minutes, seconds } = useContext(TimerContext);
 
-  const hourDegrees = (hours % 12) * 30 + (minutes / 60) * 30; // 360° / 12 hours = 30° per hour
-  const minuteDegrees = minutes * 6; // 360° / 60 minutes = 6° per minute
-  //const secondDegrees = seconds * 6; // 360° / 60 seconds = 6° per second
+  const minuteDegrees = (minutes % 60) * 6 + (seconds / 60) * 6;
+  const secondDegrees = seconds * 6;
 
   return (
     <main>
-      <section className="relative flex items-center justify-center w-[278px] h-[278px]">
-        <img className="absolute w-full h-full" src={clockFace} />
-        <motion.div
-          className="absolute"
-          style={{
-            originY: "-100%",
-            top: "50%",
-            left: "50%",
+      <section className="relative flex items-center justify-center min-w-[360px]">
+        <img src={clockFace} />
+        <motion.img
+          src={clockMinuteHandle}
+          className="absolute top-[3%] left-[47%]"
+          style={{ transformOrigin: "bottom center" }}
+          animate={{
+            rotate: minuteDegrees,
           }}
-          animate={{ rotate: hourDegrees }}
-          transition={{ duration: 0.5 }}
-        >
-          <img src={clockHourHandle} alt="Hour Hand" className="w-3 h-32" />
-        </motion.div>
-        <motion.div
-          className="absolute"
-          style={{
-            originY: "100%",
-            top: "50%",
-            left: "50%",
-            translate: "-50% -100%",
+          transition={{
+            ease: "linear",
           }}
-          animate={{ rotate: minuteDegrees }}
-          transition={{ duration: 0.5 }}
-        >
-          <img src={clockMinuteHandle} alt="Minute Hand" className="w-2 h-40" />
-        </motion.div>
+        />
+        <motion.img
+          src={clockSecondHandle}
+          className="absolute top-[3%] left-[48.5%]"
+          style={{ transformOrigin: "bottom center" }}
+          animate={{
+            rotate: secondDegrees,
+          }}
+          transition={{
+            ease: "linear",
+          }}
+        />
       </section>
     </main>
   );
